058a4dc0a21fa7939d6bc0eb8800979801c6863c
nrj022 <0602smj@gmail.com>
2022-05-04 18:14:03 +0900

    [MOD] login widget name

--- app/src/main/java/kr/co/booknuts/api/UserService.kt
+++ app/src/main/java/kr/co/booknuts/api/UserService.kt
@@ -1,4 +1,13 @@
 package kr.co.booknuts.api
 
+import kr.co.booknuts.data.LoginRequestDTO
+import kr.co.booknuts.data.User
+import retrofit2.Call
+import retrofit2.http.*
+
 interface UserService {
+    // 로그인
+    // request body - 이메일, 비밀번호
+    @POST("/auth/login")
+    fun doLogin(@Body loginInfo: LoginRequestDTO): Call<String>
 }
