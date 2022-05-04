ba97f59d97599d248e30cffcc32524eecaede251
b1urrrr <b1urrr@naver.com>
2022-05-05 01:56:06 +0900

    [ADD] signup layout implementation

---
+++ app/src/main/java/kr/co/booknuts/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.booknuts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
