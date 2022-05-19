a3d80d2d774fd42685a1c74bd3d240a8a9a01547
nrj022 <0602smj@gmail.com>
2022-05-19 14:57:57 +0900

    [ADD] home screen

---
+++ app/src/main/java/kr/co/booknuts/Adapter/PostListAdapter.kt
@@ -0,0 +1,9 @@
+package kr.co.booknuts.Adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+//public class PostListAdapter : RecyclerView.Adapter<Text> {
+//    private var mData: ArrayList<String>? = null;
+
+
+//}
