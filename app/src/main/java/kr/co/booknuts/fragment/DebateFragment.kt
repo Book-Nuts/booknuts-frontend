987906c76e4816525fa074cec01b849c60ddb6bc
b1urrrr <b1urrr@naver.com>
2022-05-21 15:23:08 +0900

    [ADD] debate fragment layout

--- app/src/main/java/kr/co/booknuts/fragment/DebateFragment.kt
+++ app/src/main/java/kr/co/booknuts/fragment/DebateFragment.kt
@@ -1,11 +1,18 @@
 package kr.co.booknuts.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kr.co.booknuts.DebateCreateActivity
+import kr.co.booknuts.MainActivity
 import kr.co.booknuts.R
+import kr.co.booknuts.databinding.ActivityDebateCreateBinding
+import kr.co.booknuts.databinding.ActivitySignUpBinding
+import kr.co.booknuts.databinding.FragmentDebateBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,20 +29,30 @@ class DebateFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val binding by lazy { FragmentDebateBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debate, container, false)
+        val binding = FragmentDebateBinding.inflate(inflater, container, false)
+
+        binding.btnAddDebate.setOnClickListener {
+            var intent = Intent(this.activity, DebateCreateActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     companion object {
